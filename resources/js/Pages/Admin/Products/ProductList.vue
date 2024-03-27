fc58ffddea31eeaecf153093715f3457d676d906
Michał Bysiewicz <michal.bysiewicz.kontakt@gmail.com>
2024-03-27 17:54:36 +0100

    Admin can add multiple products photos

--- resources/js/Pages/Admin/Products/ProductList.vue
+++ resources/js/Pages/Admin/Products/ProductList.vue
@@ -165,7 +165,7 @@ const stroeProduct = () => {
                 <div class="relative z-0 w-full mb-5 group">
                     <input
                         type="file"
-                        @input="form.product_images = $event.target.files[0]"
+                        @input="form.product_images = $event.target.files"
                         name="description"
                         id="floating_description"
                         class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
